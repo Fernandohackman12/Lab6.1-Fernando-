9305d74c0478e00c00a33a1a2c3f02a9a4f1d196
fernando12 <lic-fernando12@outlook.com>
2024-08-21 22:15:58 -0700

    new

--- js/aboutme.js
+++ js/aboutme.js
@@ -9,8 +9,3 @@ let programName = "YourProgramName";
 document.getElementById("fullname").textContent = firstName + " " + lastName;
 document.getElementById("fullName1").textContent = firstName + " " + lastName;
 document.getElementById("program1").textContent = programName;
-
-// Assuming you have an image and social media links to update in your HTML
-// Example:
-// <img id="profileImage" src="your-image-url.jpg" alt="Your Name">
-// <a id="socialMediaLink" href="https://your-social-media-profile.com">Your Social Media</a>
